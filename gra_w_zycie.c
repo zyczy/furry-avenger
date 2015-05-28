a2e53cfbc1bdd0d899b0aaf36b11e50af354ab1e
zyczy <zyczyINPG@gmail.com>
2015-05-28 14:30:06 +0200

    sdfdsf

--- gra_w_zycie.c
+++ gra_w_zycie.c
@@ -16,7 +16,7 @@ for(wypelnij_n=0; wypelnij_n<tab.wymiar_n; wypelnij_n++)
     (tab.wsk)[wypelnij_n][wypelnij_m]=0;
     }
   }
-FILE * plik;
+FILE * plik; // asdasdsdsfdsfdsfdsfdsfdsffgfdsg
 if ((plik = fopen("gra_in.txt", "r")) == NULL)
   {
   printf("Nie udalo siê otworzyc pliku.\n");
